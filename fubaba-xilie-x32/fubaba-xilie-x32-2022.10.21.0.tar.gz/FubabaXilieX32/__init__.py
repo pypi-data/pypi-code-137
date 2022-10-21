64341931ee69f236fcfce862af062428559b76f5
Tom Forbes <tom@tomforb.es>
2022-10-21 03:02:40 +0000

    {"name":"fubaba-xilie-x32","version":"2022.10.21.0","file":"fubaba-xilie-x32-2022.10.21.0.tar.gz","path":"fubaba-xilie-x32/fubaba-xilie-x32-2022.10.21.0.tar.gz"}

---
+++ fubaba-xilie-x32/fubaba-xilie-x32-2022.10.21.0.tar.gz/FubabaXilieX32/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fubaba-xilie-x32
+https://github.com/apachecn/fubaba-xilie-x32"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
