c46a16ff93116610998313d25fc2d9943e25b3cb
Tom Forbes <tom@tomforb.es>
2022-10-21 03:05:13 +0000

    {"name":"gujia-xxian-touzi","version":"2022.10.21.0","file":"gujia-xxian-touzi-2022.10.21.0.tar.gz","path":"gujia-xxian-touzi/gujia-xxian-touzi-2022.10.21.0.tar.gz"}

---
+++ gujia-xxian-touzi/gujia-xxian-touzi-2022.10.21.0.tar.gz/GujiaXxianTouzi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gujia-xxian-touzi
+https://github.com/apachecn/gujia-xxian-touzi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
