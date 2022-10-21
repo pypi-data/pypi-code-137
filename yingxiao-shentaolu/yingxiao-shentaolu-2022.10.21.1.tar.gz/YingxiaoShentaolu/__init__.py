c1022c01ac3960a26869f9b8dce6eeb4351e8ef3
Tom Forbes <tom@tomforb.es>
2022-10-21 03:41:28 +0000

    {"name":"yingxiao-shentaolu","version":"2022.10.21.1","file":"yingxiao-shentaolu-2022.10.21.1.tar.gz","path":"yingxiao-shentaolu/yingxiao-shentaolu-2022.10.21.1.tar.gz"}

---
+++ yingxiao-shentaolu/yingxiao-shentaolu-2022.10.21.1.tar.gz/YingxiaoShentaolu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingxiao-shentaolu
+https://github.com/apachecn/yingxiao-shentaolu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
