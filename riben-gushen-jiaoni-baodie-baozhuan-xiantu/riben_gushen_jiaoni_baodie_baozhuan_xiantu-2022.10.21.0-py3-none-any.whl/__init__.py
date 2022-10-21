666048a8ec32b0d7e75444828120817efd296cd2
Tom Forbes <tom@tomforb.es>
2022-10-21 03:11:43 +0000

    {"name":"riben-gushen-jiaoni-baodie-baozhuan-xiantu","version":"2022.10.21.0","file":"riben_gushen_jiaoni_baodie_baozhuan_xiantu-2022.10.21.0-py3-none-any.whl","path":"riben-gushen-jiaoni-baodie-baozhuan-xiantu/riben_gushen_jiaoni_baodie_baozhuan_xiantu-2022.10.21.0-py3-none-any.whl"}

---
+++ riben-gushen-jiaoni-baodie-baozhuan-xiantu/riben_gushen_jiaoni_baodie_baozhuan_xiantu-2022.10.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riben-gushen-jiaoni-baodie-baozhuan-xiantu
+https://github.com/apachecn/riben-gushen-jiaoni-baodie-baozhuan-xiantu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
