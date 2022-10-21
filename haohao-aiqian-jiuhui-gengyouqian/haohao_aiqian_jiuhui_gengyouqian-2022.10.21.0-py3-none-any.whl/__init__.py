441e51de8d07bc17d77ac75d1e0283ec138b2485
Tom Forbes <tom@tomforb.es>
2022-10-21 03:06:28 +0000

    {"name":"haohao-aiqian-jiuhui-gengyouqian","version":"2022.10.21.0","file":"haohao_aiqian_jiuhui_gengyouqian-2022.10.21.0-py3-none-any.whl","path":"haohao-aiqian-jiuhui-gengyouqian/haohao_aiqian_jiuhui_gengyouqian-2022.10.21.0-py3-none-any.whl"}

---
+++ haohao-aiqian-jiuhui-gengyouqian/haohao_aiqian_jiuhui_gengyouqian-2022.10.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""haohao-aiqian-jiuhui-gengyouqian
+https://github.com/apachecn/haohao-aiqian-jiuhui-gengyouqian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
