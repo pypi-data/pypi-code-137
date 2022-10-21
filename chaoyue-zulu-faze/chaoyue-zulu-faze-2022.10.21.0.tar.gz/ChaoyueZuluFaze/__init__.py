45e0aa712630534ba1ac27a9ad0eedfd63fe8a9e
Tom Forbes <tom@tomforb.es>
2022-10-21 02:59:31 +0000

    {"name":"chaoyue-zulu-faze","version":"2022.10.21.0","file":"chaoyue-zulu-faze-2022.10.21.0.tar.gz","path":"chaoyue-zulu-faze/chaoyue-zulu-faze-2022.10.21.0.tar.gz"}

---
+++ chaoyue-zulu-faze/chaoyue-zulu-faze-2022.10.21.0.tar.gz/ChaoyueZuluFaze/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chaoyue-zulu-faze
+https://github.com/apachecn/chaoyue-zulu-faze"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
