01840bc0efb2949ea8ea61795df25601d0f283a1
Tom Forbes <tom@tomforb.es>
2022-10-21 03:37:01 +0000

    {"name":"riben-gushen-jiaoni-zenmemai","version":"2022.10.21.1","file":"riben_gushen_jiaoni_zenmemai-2022.10.21.1-py3-none-any.whl","path":"riben-gushen-jiaoni-zenmemai/riben_gushen_jiaoni_zenmemai-2022.10.21.1-py3-none-any.whl"}

---
+++ riben-gushen-jiaoni-zenmemai/riben_gushen_jiaoni_zenmemai-2022.10.21.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riben-gushen-jiaoni-zenmemai
+https://github.com/apachecn/riben-gushen-jiaoni-zenmemai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
