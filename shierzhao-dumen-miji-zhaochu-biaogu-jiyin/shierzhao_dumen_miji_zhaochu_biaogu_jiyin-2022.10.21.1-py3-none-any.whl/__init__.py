0deee8ffe8168782c035799dcdd025e7ed6ca444
Tom Forbes <tom@tomforb.es>
2022-10-21 03:38:33 +0000

    {"name":"shierzhao-dumen-miji-zhaochu-biaogu-jiyin","version":"2022.10.21.1","file":"shierzhao_dumen_miji_zhaochu_biaogu_jiyin-2022.10.21.1-py3-none-any.whl","path":"shierzhao-dumen-miji-zhaochu-biaogu-jiyin/shierzhao_dumen_miji_zhaochu_biaogu_jiyin-2022.10.21.1-py3-none-any.whl"}

---
+++ shierzhao-dumen-miji-zhaochu-biaogu-jiyin/shierzhao_dumen_miji_zhaochu_biaogu_jiyin-2022.10.21.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shierzhao-dumen-miji-zhaochu-biaogu-jiyin
+https://github.com/apachecn/shierzhao-dumen-miji-zhaochu-biaogu-jiyin"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
