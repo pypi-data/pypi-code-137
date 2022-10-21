2a8d398a336f654d55c2b3bc15a48ab15294cf91
Tom Forbes <tom@tomforb.es>
2022-10-21 03:37:29 +0000

    {"name":"rongzide-zuigao-jingjie-shang","version":"2022.10.21.1","file":"rongzide_zuigao_jingjie_shang-2022.10.21.1-py3-none-any.whl","path":"rongzide-zuigao-jingjie-shang/rongzide_zuigao_jingjie_shang-2022.10.21.1-py3-none-any.whl"}

---
+++ rongzide-zuigao-jingjie-shang/rongzide_zuigao_jingjie_shang-2022.10.21.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""rongzide-zuigao-jingjie-shang
+https://github.com/apachecn/rongzide-zuigao-jingjie-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
