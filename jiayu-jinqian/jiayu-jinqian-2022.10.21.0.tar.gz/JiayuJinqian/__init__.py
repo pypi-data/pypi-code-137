765953007fdeb5dce370fffce5fec19257c19401
Tom Forbes <tom@tomforb.es>
2022-10-21 03:07:16 +0000

    {"name":"jiayu-jinqian","version":"2022.10.21.0","file":"jiayu-jinqian-2022.10.21.0.tar.gz","path":"jiayu-jinqian/jiayu-jinqian-2022.10.21.0.tar.gz"}

---
+++ jiayu-jinqian/jiayu-jinqian-2022.10.21.0.tar.gz/JiayuJinqian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiayu-jinqian
+https://github.com/apachecn/jiayu-jinqian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
