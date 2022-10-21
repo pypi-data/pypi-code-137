4e5131a16abf894fc041d9e2b2a5be722f3f0776
Tom Forbes <tom@tomforb.es>
2022-10-21 03:36:35 +0000

    {"name":"riben-dingjian-wushisange-jinqian-zhexue","version":"2022.10.21.1","file":"riben-dingjian-wushisange-jinqian-zhexue-2022.10.21.1.tar.gz","path":"riben-dingjian-wushisange-jinqian-zhexue/riben-dingjian-wushisange-jinqian-zhexue-2022.10.21.1.tar.gz"}

---
+++ riben-dingjian-wushisange-jinqian-zhexue/riben-dingjian-wushisange-jinqian-zhexue-2022.10.21.1.tar.gz/RibenDingjianWushisangeJinqianZhexue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riben-dingjian-wushisange-jinqian-zhexue
+https://github.com/apachecn/riben-dingjian-wushisange-jinqian-zhexue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
