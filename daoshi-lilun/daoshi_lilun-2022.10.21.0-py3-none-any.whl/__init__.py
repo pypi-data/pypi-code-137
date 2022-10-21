b1e984fd289b4203b1edf3bbf20c58925d0296f4
Tom Forbes <tom@tomforb.es>
2022-10-21 03:00:33 +0000

    {"name":"daoshi-lilun","version":"2022.10.21.0","file":"daoshi_lilun-2022.10.21.0-py3-none-any.whl","path":"daoshi-lilun/daoshi_lilun-2022.10.21.0-py3-none-any.whl"}

---
+++ daoshi-lilun/daoshi_lilun-2022.10.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daoshi-lilun
+https://github.com/apachecn/daoshi-lilun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
