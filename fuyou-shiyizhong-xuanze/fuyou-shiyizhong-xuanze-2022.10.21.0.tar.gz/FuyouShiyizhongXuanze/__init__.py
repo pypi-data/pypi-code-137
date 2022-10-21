ebc4a466d06eaa27aad910a5f19a7beb65ffddd0
Tom Forbes <tom@tomforb.es>
2022-10-21 03:04:02 +0000

    {"name":"fuyou-shiyizhong-xuanze","version":"2022.10.21.0","file":"fuyou-shiyizhong-xuanze-2022.10.21.0.tar.gz","path":"fuyou-shiyizhong-xuanze/fuyou-shiyizhong-xuanze-2022.10.21.0.tar.gz"}

---
+++ fuyou-shiyizhong-xuanze/fuyou-shiyizhong-xuanze-2022.10.21.0.tar.gz/FuyouShiyizhongXuanze/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fuyou-shiyizhong-xuanze
+https://github.com/apachecn/fuyou-shiyizhong-xuanze"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
