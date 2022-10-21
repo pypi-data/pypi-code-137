5516a80bc36592509025da3af189a72084171929
Tom Forbes <tom@tomforb.es>
2022-10-21 03:32:29 +0000

    {"name":"nixiang-touzi-celue","version":"2022.10.21.1","file":"nixiang-touzi-celue-2022.10.21.1.tar.gz","path":"nixiang-touzi-celue/nixiang-touzi-celue-2022.10.21.1.tar.gz"}

---
+++ nixiang-touzi-celue/nixiang-touzi-celue-2022.10.21.1.tar.gz/NixiangTouziCelue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nixiang-touzi-celue
+https://github.com/apachecn/nixiang-touzi-celue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
