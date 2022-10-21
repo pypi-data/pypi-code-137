79ac5cc19029f298c8be60e9baf5cb88093f8009
Tom Forbes <tom@tomforb.es>
2022-10-21 03:30:35 +0000

    {"name":"liushisan-zhangtu-jiaoni-zhangdie-dounengzhuan","version":"2022.10.21.1","file":"liushisan_zhangtu_jiaoni_zhangdie_dounengzhuan-2022.10.21.1-py3-none-any.whl","path":"liushisan-zhangtu-jiaoni-zhangdie-dounengzhuan/liushisan_zhangtu_jiaoni_zhangdie_dounengzhuan-2022.10.21.1-py3-none-any.whl"}

---
+++ liushisan-zhangtu-jiaoni-zhangdie-dounengzhuan/liushisan_zhangtu_jiaoni_zhangdie_dounengzhuan-2022.10.21.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liushisan-zhangtu-jiaoni-zhangdie-dounengzhuan
+https://github.com/apachecn/liushisan-zhangtu-jiaoni-zhangdie-dounengzhuan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
