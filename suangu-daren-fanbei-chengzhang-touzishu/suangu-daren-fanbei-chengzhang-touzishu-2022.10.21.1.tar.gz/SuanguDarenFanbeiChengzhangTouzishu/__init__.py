f0f7aca92196cc95807d263bb3f58f15f022f0e9
Tom Forbes <tom@tomforb.es>
2022-10-21 03:39:20 +0000

    {"name":"suangu-daren-fanbei-chengzhang-touzishu","version":"2022.10.21.1","file":"suangu-daren-fanbei-chengzhang-touzishu-2022.10.21.1.tar.gz","path":"suangu-daren-fanbei-chengzhang-touzishu/suangu-daren-fanbei-chengzhang-touzishu-2022.10.21.1.tar.gz"}

---
+++ suangu-daren-fanbei-chengzhang-touzishu/suangu-daren-fanbei-chengzhang-touzishu-2022.10.21.1.tar.gz/SuanguDarenFanbeiChengzhangTouzishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""suangu-daren-fanbei-chengzhang-touzishu
+https://github.com/apachecn/suangu-daren-fanbei-chengzhang-touzishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.1"
