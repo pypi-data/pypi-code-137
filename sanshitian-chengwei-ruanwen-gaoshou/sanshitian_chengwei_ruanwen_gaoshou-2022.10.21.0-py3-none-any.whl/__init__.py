97efcb08a695c6dd71c57c5754ded254e07141fb
Tom Forbes <tom@tomforb.es>
2022-10-21 03:13:21 +0000

    {"name":"sanshitian-chengwei-ruanwen-gaoshou","version":"2022.10.21.0","file":"sanshitian_chengwei_ruanwen_gaoshou-2022.10.21.0-py3-none-any.whl","path":"sanshitian-chengwei-ruanwen-gaoshou/sanshitian_chengwei_ruanwen_gaoshou-2022.10.21.0-py3-none-any.whl"}

---
+++ sanshitian-chengwei-ruanwen-gaoshou/sanshitian_chengwei_ruanwen_gaoshou-2022.10.21.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sanshitian-chengwei-ruanwen-gaoshou
+https://github.com/apachecn/sanshitian-chengwei-ruanwen-gaoshou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.21.0"
