0235f5802b920ac542d9b96d9deb367445b29649
Tom Forbes <tom@tomforb.es>
2022-10-22 02:52:33 +0000

    {"name":"caipiao-yuqimen","version":"2022.10.22.0","file":"caipiao-yuqimen-2022.10.22.0.tar.gz","path":"caipiao-yuqimen/caipiao-yuqimen-2022.10.22.0.tar.gz"}

---
+++ caipiao-yuqimen/caipiao-yuqimen-2022.10.22.0.tar.gz/CaipiaoYuqimen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""caipiao-yuqimen
+https://github.com/apachecn/caipiao-yuqimen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
