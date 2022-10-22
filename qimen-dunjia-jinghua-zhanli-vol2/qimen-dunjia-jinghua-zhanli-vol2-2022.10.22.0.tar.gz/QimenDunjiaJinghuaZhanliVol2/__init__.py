50acb30a93a289d11cf6e7442d93bcc9220274bf
Tom Forbes <tom@tomforb.es>
2022-10-22 03:01:52 +0000

    {"name":"qimen-dunjia-jinghua-zhanli-vol2","version":"2022.10.22.0","file":"qimen-dunjia-jinghua-zhanli-vol2-2022.10.22.0.tar.gz","path":"qimen-dunjia-jinghua-zhanli-vol2/qimen-dunjia-jinghua-zhanli-vol2-2022.10.22.0.tar.gz"}

---
+++ qimen-dunjia-jinghua-zhanli-vol2/qimen-dunjia-jinghua-zhanli-vol2-2022.10.22.0.tar.gz/QimenDunjiaJinghuaZhanliVol2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-dunjia-jinghua-zhanli-vol2
+https://github.com/apachecn/qimen-dunjia-jinghua-zhanli-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
