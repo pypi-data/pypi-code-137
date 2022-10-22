4fdedf444bbbdee3e5bc728d6d183e826f06cd60
Tom Forbes <tom@tomforb.es>
2022-10-22 02:55:57 +0000

    {"name":"jianxiulan-boshi-fansi-xinyuanxing","version":"2022.10.22.0","file":"jianxiulan-boshi-fansi-xinyuanxing-2022.10.22.0.tar.gz","path":"jianxiulan-boshi-fansi-xinyuanxing/jianxiulan-boshi-fansi-xinyuanxing-2022.10.22.0.tar.gz"}

---
+++ jianxiulan-boshi-fansi-xinyuanxing/jianxiulan-boshi-fansi-xinyuanxing-2022.10.22.0.tar.gz/JianxiulanBoshiFansiXinyuanxing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jianxiulan-boshi-fansi-xinyuanxing
+https://github.com/apachecn/jianxiulan-boshi-fansi-xinyuanxing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
