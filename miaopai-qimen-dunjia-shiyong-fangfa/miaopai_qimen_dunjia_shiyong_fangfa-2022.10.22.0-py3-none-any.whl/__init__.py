ea79e8761fcf1bad4958278931a2f791c1ae2ebf
Tom Forbes <tom@tomforb.es>
2022-10-22 03:01:25 +0000

    {"name":"miaopai-qimen-dunjia-shiyong-fangfa","version":"2022.10.22.0","file":"miaopai_qimen_dunjia_shiyong_fangfa-2022.10.22.0-py3-none-any.whl","path":"miaopai-qimen-dunjia-shiyong-fangfa/miaopai_qimen_dunjia_shiyong_fangfa-2022.10.22.0-py3-none-any.whl"}

---
+++ miaopai-qimen-dunjia-shiyong-fangfa/miaopai_qimen_dunjia_shiyong_fangfa-2022.10.22.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""miaopai-qimen-dunjia-shiyong-fangfa
+https://github.com/apachecn/miaopai-qimen-dunjia-shiyong-fangfa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
