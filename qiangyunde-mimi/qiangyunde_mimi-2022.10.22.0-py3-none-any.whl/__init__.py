0aa62432887ffaec8c5efea6ccadc62dcbe32fff
Tom Forbes <tom@tomforb.es>
2022-10-22 03:01:35 +0000

    {"name":"qiangyunde-mimi","version":"2022.10.22.0","file":"qiangyunde_mimi-2022.10.22.0-py3-none-any.whl","path":"qiangyunde-mimi/qiangyunde_mimi-2022.10.22.0-py3-none-any.whl"}

---
+++ qiangyunde-mimi/qiangyunde_mimi-2022.10.22.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qiangyunde-mimi
+https://github.com/apachecn/qiangyunde-mimi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
