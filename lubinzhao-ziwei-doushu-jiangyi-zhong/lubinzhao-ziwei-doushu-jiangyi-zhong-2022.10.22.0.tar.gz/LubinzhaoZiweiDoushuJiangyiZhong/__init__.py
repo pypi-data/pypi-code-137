97e8b41c148f31614721b17879c66060d01fe6fb
Tom Forbes <tom@tomforb.es>
2022-10-22 02:59:43 +0000

    {"name":"lubinzhao-ziwei-doushu-jiangyi-zhong","version":"2022.10.22.0","file":"lubinzhao-ziwei-doushu-jiangyi-zhong-2022.10.22.0.tar.gz","path":"lubinzhao-ziwei-doushu-jiangyi-zhong/lubinzhao-ziwei-doushu-jiangyi-zhong-2022.10.22.0.tar.gz"}

---
+++ lubinzhao-ziwei-doushu-jiangyi-zhong/lubinzhao-ziwei-doushu-jiangyi-zhong-2022.10.22.0.tar.gz/LubinzhaoZiweiDoushuJiangyiZhong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lubinzhao-ziwei-doushu-jiangyi-zhong
+https://github.com/apachecn/lubinzhao-ziwei-doushu-jiangyi-zhong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
