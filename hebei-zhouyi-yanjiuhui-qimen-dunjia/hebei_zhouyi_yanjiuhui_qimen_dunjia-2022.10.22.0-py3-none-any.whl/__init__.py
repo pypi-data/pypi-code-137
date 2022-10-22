51571e8bb6724eb4b292144c07a8fceac88a0e00
Tom Forbes <tom@tomforb.es>
2022-10-22 02:55:18 +0000

    {"name":"hebei-zhouyi-yanjiuhui-qimen-dunjia","version":"2022.10.22.0","file":"hebei_zhouyi_yanjiuhui_qimen_dunjia-2022.10.22.0-py3-none-any.whl","path":"hebei-zhouyi-yanjiuhui-qimen-dunjia/hebei_zhouyi_yanjiuhui_qimen_dunjia-2022.10.22.0-py3-none-any.whl"}

---
+++ hebei-zhouyi-yanjiuhui-qimen-dunjia/hebei_zhouyi_yanjiuhui_qimen_dunjia-2022.10.22.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hebei-zhouyi-yanjiuhui-qimen-dunjia
+https://github.com/apachecn/hebei-zhouyi-yanjiuhui-qimen-dunjia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.22.0"
