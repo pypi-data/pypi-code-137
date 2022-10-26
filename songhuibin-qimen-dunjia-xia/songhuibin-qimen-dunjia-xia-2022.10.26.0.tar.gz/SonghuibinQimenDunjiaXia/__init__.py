76afb0bafa94facdd7d3c49462c6b181845118d5
Tom Forbes <tom@tomforb.es>
2022-10-26 13:51:27 +0000

    {"name":"songhuibin-qimen-dunjia-xia","version":"2022.10.26.0","file":"songhuibin-qimen-dunjia-xia-2022.10.26.0.tar.gz","path":"songhuibin-qimen-dunjia-xia/songhuibin-qimen-dunjia-xia-2022.10.26.0.tar.gz"}

---
+++ songhuibin-qimen-dunjia-xia/songhuibin-qimen-dunjia-xia-2022.10.26.0.tar.gz/SonghuibinQimenDunjiaXia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""songhuibin-qimen-dunjia-xia
+https://github.com/apachecn/songhuibin-qimen-dunjia-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
