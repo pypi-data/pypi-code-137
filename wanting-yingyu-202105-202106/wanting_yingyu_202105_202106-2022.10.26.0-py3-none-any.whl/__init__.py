8f21f72ff87076461bac9bd6278907ed1fcca696
Tom Forbes <tom@tomforb.es>
2022-10-26 03:03:43 +0000

    {"name":"wanting-yingyu-202105-202106","version":"2022.10.26.0","file":"wanting_yingyu_202105_202106-2022.10.26.0-py3-none-any.whl","path":"wanting-yingyu-202105-202106/wanting_yingyu_202105_202106-2022.10.26.0-py3-none-any.whl"}

---
+++ wanting-yingyu-202105-202106/wanting_yingyu_202105_202106-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wanting-yingyu-202105-202106
+https://github.com/apachecn/wanting-yingyu-202105-202106"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
