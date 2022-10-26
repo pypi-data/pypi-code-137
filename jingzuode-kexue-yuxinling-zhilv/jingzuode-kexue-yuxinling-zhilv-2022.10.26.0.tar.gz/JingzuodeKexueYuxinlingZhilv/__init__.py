445871e2530c6c9af18828c8f34fcbd46e736a30
Tom Forbes <tom@tomforb.es>
2022-10-26 13:44:13 +0000

    {"name":"jingzuode-kexue-yuxinling-zhilv","version":"2022.10.26.0","file":"jingzuode-kexue-yuxinling-zhilv-2022.10.26.0.tar.gz","path":"jingzuode-kexue-yuxinling-zhilv/jingzuode-kexue-yuxinling-zhilv-2022.10.26.0.tar.gz"}

---
+++ jingzuode-kexue-yuxinling-zhilv/jingzuode-kexue-yuxinling-zhilv-2022.10.26.0.tar.gz/JingzuodeKexueYuxinlingZhilv/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingzuode-kexue-yuxinling-zhilv
+https://github.com/apachecn/jingzuode-kexue-yuxinling-zhilv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
