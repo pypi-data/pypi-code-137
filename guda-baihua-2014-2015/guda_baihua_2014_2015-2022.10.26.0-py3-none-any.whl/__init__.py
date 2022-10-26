c50c20c991d1b9619e46ca4445a58de89a65f1ba
Tom Forbes <tom@tomforb.es>
2022-10-26 02:47:47 +0000

    {"name":"guda-baihua-2014-2015","version":"2022.10.26.0","file":"guda_baihua_2014_2015-2022.10.26.0-py3-none-any.whl","path":"guda-baihua-2014-2015/guda_baihua_2014_2015-2022.10.26.0-py3-none-any.whl"}

---
+++ guda-baihua-2014-2015/guda_baihua_2014_2015-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guda-baihua-2014-2015
+https://github.com/apachecn/guda-baihua-2014-2015"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
