5fdfdbcc70dd69034e461ea45375e69c95c1be96
Tom Forbes <tom@tomforb.es>
2022-10-26 13:58:34 +0000

    {"name":"yizhan-xinjifa","version":"2022.10.26.0","file":"yizhan-xinjifa-2022.10.26.0.tar.gz","path":"yizhan-xinjifa/yizhan-xinjifa-2022.10.26.0.tar.gz"}

---
+++ yizhan-xinjifa/yizhan-xinjifa-2022.10.26.0.tar.gz/YizhanXinjifa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yizhan-xinjifa
+https://github.com/apachecn/yizhan-xinjifa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
