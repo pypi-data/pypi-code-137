77cbdca53ab9787513475a59679fcdedb25df84c
Tom Forbes <tom@tomforb.es>
2022-10-26 13:43:38 +0000

    {"name":"fuzhou-yingyong-miaofa-quanshu","version":"2022.10.26.0","file":"fuzhou-yingyong-miaofa-quanshu-2022.10.26.0.tar.gz","path":"fuzhou-yingyong-miaofa-quanshu/fuzhou-yingyong-miaofa-quanshu-2022.10.26.0.tar.gz"}

---
+++ fuzhou-yingyong-miaofa-quanshu/fuzhou-yingyong-miaofa-quanshu-2022.10.26.0.tar.gz/FuzhouYingyongMiaofaQuanshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fuzhou-yingyong-miaofa-quanshu
+https://github.com/apachecn/fuzhou-yingyong-miaofa-quanshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
