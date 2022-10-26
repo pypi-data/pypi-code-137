6fcf10ad5f149d05a105897b81e81b9f9122b501
Tom Forbes <tom@tomforb.es>
2022-10-26 13:40:21 +0000

    {"name":"doushu-yinan-100-wenda-gudianpian","version":"2022.10.26.0","file":"doushu_yinan_100_wenda_gudianpian-2022.10.26.0-py3-none-any.whl","path":"doushu-yinan-100-wenda-gudianpian/doushu_yinan_100_wenda_gudianpian-2022.10.26.0-py3-none-any.whl"}

---
+++ doushu-yinan-100-wenda-gudianpian/doushu_yinan_100_wenda_gudianpian-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""doushu-yinan-100-wenda-gudianpian
+https://github.com/apachecn/doushu-yinan-100-wenda-gudianpian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
