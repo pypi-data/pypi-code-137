5ac8364958454faee3d542ef347ff59af1398fc1
Tom Forbes <tom@tomforb.es>
2022-10-26 13:59:52 +0000

    {"name":"ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang","version":"2022.10.26.0","file":"ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang-2022.10.26.0.tar.gz","path":"ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang/ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang-2022.10.26.0.tar.gz"}

---
+++ ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang/ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang-2022.10.26.0.tar.gz/ZiweiDoushuLunmingJiqiaoJishiliJiexiShang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang
+https://github.com/apachecn/ziwei-doushu-lunming-jiqiao-jishili-jiexi-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
