b8c463be89950d3dccbdd76c7fec8819de14660d
Tom Forbes <tom@tomforb.es>
2022-10-26 17:56:44 +0000

    {"name":"liuwei-yasi-pindao-201603-201609","version":"2022.10.27.0","file":"liuwei_yasi_pindao_201603_201609-2022.10.27.0-py3-none-any.whl","path":"liuwei-yasi-pindao-201603-201609/liuwei_yasi_pindao_201603_201609-2022.10.27.0-py3-none-any.whl"}

---
+++ liuwei-yasi-pindao-201603-201609/liuwei_yasi_pindao_201603_201609-2022.10.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuwei-yasi-pindao-201603-201609
+https://github.com/apachecn/liuwei-yasi-pindao-201603-201609"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
