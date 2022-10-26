b533e4cbd1052d48294a7e8d01dbfe6e544fd1f0
Tom Forbes <tom@tomforb.es>
2022-10-26 13:42:40 +0000

    {"name":"feixing-ziwei-doushu-shuoming-shang","version":"2022.10.26.0","file":"feixing_ziwei_doushu_shuoming_shang-2022.10.26.0-py3-none-any.whl","path":"feixing-ziwei-doushu-shuoming-shang/feixing_ziwei_doushu_shuoming_shang-2022.10.26.0-py3-none-any.whl"}

---
+++ feixing-ziwei-doushu-shuoming-shang/feixing_ziwei_doushu_shuoming_shang-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""feixing-ziwei-doushu-shuoming-shang
+https://github.com/apachecn/feixing-ziwei-doushu-shuoming-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
