180cc612f6b91c4d649aed01965fe65d7fbb4ac4
Tom Forbes <tom@tomforb.es>
2022-10-26 13:52:31 +0000

    {"name":"tounaode-dongxi","version":"2022.10.26.0","file":"tounaode_dongxi-2022.10.26.0-py3-none-any.whl","path":"tounaode-dongxi/tounaode_dongxi-2022.10.26.0-py3-none-any.whl"}

---
+++ tounaode-dongxi/tounaode_dongxi-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tounaode-dongxi
+https://github.com/apachecn/tounaode-dongxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
