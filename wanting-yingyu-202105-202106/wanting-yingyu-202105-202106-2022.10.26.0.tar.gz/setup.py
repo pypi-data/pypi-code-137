3c80dd1f18270cfa63ab0928d4f03cacbc79a17d
Tom Forbes <tom@tomforb.es>
2022-10-26 03:04:26 +0000

    {"name":"wanting-yingyu-202105-202106","version":"2022.10.26.0","file":"wanting-yingyu-202105-202106-2022.10.26.0.tar.gz","path":"wanting-yingyu-202105-202106/wanting-yingyu-202105-202106-2022.10.26.0.tar.gz"}

---
+++ wanting-yingyu-202105-202106/wanting-yingyu-202105-202106-2022.10.26.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import WantingYingyu202105202106
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('WantingYingyu202105202106'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="wanting-yingyu-202105-202106",
+    version=WantingYingyu202105202106.__version__,
+    url="https://github.com/apachecn/wanting-yingyu-202105-202106",
+    author=WantingYingyu202105202106.__author__,
+    author_email=WantingYingyu202105202106.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="晚听英语 2021.5~2021.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "wanting-yingyu-202105-202106=WantingYingyu202105202106.__main__:main",
+            "WantingYingyu202105202106=WantingYingyu202105202106.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
