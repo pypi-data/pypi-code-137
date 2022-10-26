99491ff831b0b72646692a551460a337c98bc409
Tom Forbes <tom@tomforb.es>
2022-10-26 02:54:50 +0000

    {"name":"liuwei-yasi-pindao-202006-202101","version":"2022.10.26.0","file":"liuwei-yasi-pindao-202006-202101-2022.10.26.0.tar.gz","path":"liuwei-yasi-pindao-202006-202101/liuwei-yasi-pindao-202006-202101-2022.10.26.0.tar.gz"}

---
+++ liuwei-yasi-pindao-202006-202101/liuwei-yasi-pindao-202006-202101-2022.10.26.0.tar.gz/LiuweiYasiPindao202006202101/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuwei-yasi-pindao-202006-202101
+https://github.com/apachecn/liuwei-yasi-pindao-202006-202101"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
