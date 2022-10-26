23d0b55dbcbcd31c95a426fe11b5aec1a2f8d7cc
Tom Forbes <tom@tomforb.es>
2022-10-26 03:19:04 +0000

    {"name":"yingyu-yanshuo-youxuan-202010-202103","version":"2022.10.26.0","file":"yingyu-yanshuo-youxuan-202010-202103-2022.10.26.0.tar.gz","path":"yingyu-yanshuo-youxuan-202010-202103/yingyu-yanshuo-youxuan-202010-202103-2022.10.26.0.tar.gz"}

---
+++ yingyu-yanshuo-youxuan-202010-202103/yingyu-yanshuo-youxuan-202010-202103-2022.10.26.0.tar.gz/YingyuYanshuoYouxuan202010202103/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-yanshuo-youxuan-202010-202103
+https://github.com/apachecn/yingyu-yanshuo-youxuan-202010-202103"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
