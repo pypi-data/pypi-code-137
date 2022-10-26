b014ab34dd0bc5a53f2b4f5aaaaa81f6392f8327
Tom Forbes <tom@tomforb.es>
2022-10-26 13:53:19 +0000

    {"name":"wangtingzhi-ziwei-doushu-quanji-vol3","version":"2022.10.26.0","file":"wangtingzhi-ziwei-doushu-quanji-vol3-2022.10.26.0.tar.gz","path":"wangtingzhi-ziwei-doushu-quanji-vol3/wangtingzhi-ziwei-doushu-quanji-vol3-2022.10.26.0.tar.gz"}

---
+++ wangtingzhi-ziwei-doushu-quanji-vol3/wangtingzhi-ziwei-doushu-quanji-vol3-2022.10.26.0.tar.gz/WangtingzhiZiweiDoushuQuanjiVol3/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wangtingzhi-ziwei-doushu-quanji-vol3
+https://github.com/apachecn/wangtingzhi-ziwei-doushu-quanji-vol3"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
