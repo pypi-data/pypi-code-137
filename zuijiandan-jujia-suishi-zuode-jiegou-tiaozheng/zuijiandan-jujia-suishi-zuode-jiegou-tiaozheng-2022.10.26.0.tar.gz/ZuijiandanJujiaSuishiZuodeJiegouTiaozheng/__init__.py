a0c19994836f7804d6e3d60feb1ac40393ea13dd
Tom Forbes <tom@tomforb.es>
2022-10-26 14:01:44 +0000

    {"name":"zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng","version":"2022.10.26.0","file":"zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng-2022.10.26.0.tar.gz","path":"zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng/zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng-2022.10.26.0.tar.gz"}

---
+++ zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng/zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng-2022.10.26.0.tar.gz/ZuijiandanJujiaSuishiZuodeJiegouTiaozheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng
+https://github.com/apachecn/zuijiandan-jujia-suishi-zuode-jiegou-tiaozheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
