a6cabb8698fefa507ae828c4fb810229c279fe92
Tom Forbes <tom@tomforb.es>
2022-10-26 13:43:59 +0000

    {"name":"jiangtaigong-qimen-dunjia-fengshui","version":"2022.10.26.0","file":"jiangtaigong-qimen-dunjia-fengshui-2022.10.26.0.tar.gz","path":"jiangtaigong-qimen-dunjia-fengshui/jiangtaigong-qimen-dunjia-fengshui-2022.10.26.0.tar.gz"}

---
+++ jiangtaigong-qimen-dunjia-fengshui/jiangtaigong-qimen-dunjia-fengshui-2022.10.26.0.tar.gz/JiangtaigongQimenDunjiaFengshui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiangtaigong-qimen-dunjia-fengshui
+https://github.com/apachecn/jiangtaigong-qimen-dunjia-fengshui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
