adc6695f89de103dd6964aa029c20f98ec3e4d01
Tom Forbes <tom@tomforb.es>
2022-10-26 13:52:19 +0000

    {"name":"touguo-yundong-heliaoxiao-zishi-jiegou-tiaozheng","version":"2022.10.26.0","file":"touguo_yundong_heliaoxiao_zishi_jiegou_tiaozheng-2022.10.26.0-py3-none-any.whl","path":"touguo-yundong-heliaoxiao-zishi-jiegou-tiaozheng/touguo_yundong_heliaoxiao_zishi_jiegou_tiaozheng-2022.10.26.0-py3-none-any.whl"}

---
+++ touguo-yundong-heliaoxiao-zishi-jiegou-tiaozheng/touguo_yundong_heliaoxiao_zishi_jiegou_tiaozheng-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""touguo-yundong-heliaoxiao-zishi-jiegou-tiaozheng
+https://github.com/apachecn/touguo-yundong-heliaoxiao-zishi-jiegou-tiaozheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
