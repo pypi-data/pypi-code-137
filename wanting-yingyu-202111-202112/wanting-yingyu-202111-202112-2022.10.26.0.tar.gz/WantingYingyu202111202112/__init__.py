cd742d0a22e4b9f73ea1e9cb175ca3d54708707b
Tom Forbes <tom@tomforb.es>
2022-10-26 03:08:58 +0000

    {"name":"wanting-yingyu-202111-202112","version":"2022.10.26.0","file":"wanting-yingyu-202111-202112-2022.10.26.0.tar.gz","path":"wanting-yingyu-202111-202112/wanting-yingyu-202111-202112-2022.10.26.0.tar.gz"}

---
+++ wanting-yingyu-202111-202112/wanting-yingyu-202111-202112-2022.10.26.0.tar.gz/WantingYingyu202111202112/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wanting-yingyu-202111-202112
+https://github.com/apachecn/wanting-yingyu-202111-202112"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
