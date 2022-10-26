a957eca61d8d829e57cdd68e02c8aa1f3fc57c07
Tom Forbes <tom@tomforb.es>
2022-10-26 03:01:06 +0000

    {"name":"tianjian-xiaozhan-2018-202003","version":"2022.10.26.0","file":"tianjian-xiaozhan-2018-202003-2022.10.26.0.tar.gz","path":"tianjian-xiaozhan-2018-202003/tianjian-xiaozhan-2018-202003-2022.10.26.0.tar.gz"}

---
+++ tianjian-xiaozhan-2018-202003/tianjian-xiaozhan-2018-202003-2022.10.26.0.tar.gz/TianjianXiaozhan2018202003/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tianjian-xiaozhan-2018-202003
+https://github.com/apachecn/tianjian-xiaozhan-2018-202003"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
