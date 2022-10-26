c31c9c0c5ff5779aeb597a9fb3cf7d0621c600a9
Tom Forbes <tom@tomforb.es>
2022-10-26 18:11:23 +0000

    {"name":"sangsangjun-202111-202201","version":"2022.10.27.0","file":"sangsangjun-202111-202201-2022.10.27.0.tar.gz","path":"sangsangjun-202111-202201/sangsangjun-202111-202201-2022.10.27.0.tar.gz"}

---
+++ sangsangjun-202111-202201/sangsangjun-202111-202201-2022.10.27.0.tar.gz/Sangsangjun202111202201/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sangsangjun-202111-202201
+https://github.com/apachecn/sangsangjun-202111-202201"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
