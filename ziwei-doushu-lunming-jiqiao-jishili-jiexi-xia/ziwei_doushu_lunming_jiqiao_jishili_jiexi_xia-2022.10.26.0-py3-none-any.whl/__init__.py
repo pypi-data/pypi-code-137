fd8749cbb3bd2e3023c136c0c10a93329cd8b7c6
Tom Forbes <tom@tomforb.es>
2022-10-26 14:00:03 +0000

    {"name":"ziwei-doushu-lunming-jiqiao-jishili-jiexi-xia","version":"2022.10.26.0","file":"ziwei_doushu_lunming_jiqiao_jishili_jiexi_xia-2022.10.26.0-py3-none-any.whl","path":"ziwei-doushu-lunming-jiqiao-jishili-jiexi-xia/ziwei_doushu_lunming_jiqiao_jishili_jiexi_xia-2022.10.26.0-py3-none-any.whl"}

---
+++ ziwei-doushu-lunming-jiqiao-jishili-jiexi-xia/ziwei_doushu_lunming_jiqiao_jishili_jiexi_xia-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-lunming-jiqiao-jishili-jiexi-xia
+https://github.com/apachecn/ziwei-doushu-lunming-jiqiao-jishili-jiexi-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
