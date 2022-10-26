3a9d16793ec1fa730fd13912fdd33a4c27c07741
Tom Forbes <tom@tomforb.es>
2022-10-26 03:01:34 +0000

    {"name":"tianjian-xiaozhan-202007-202010","version":"2022.10.26.0","file":"tianjian_xiaozhan_202007_202010-2022.10.26.0-py3-none-any.whl","path":"tianjian-xiaozhan-202007-202010/tianjian_xiaozhan_202007_202010-2022.10.26.0-py3-none-any.whl"}

---
+++ tianjian-xiaozhan-202007-202010/tianjian_xiaozhan_202007_202010-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tianjian-xiaozhan-202007-202010
+https://github.com/apachecn/tianjian-xiaozhan-202007-202010"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
