b225a1c9e6dd7f8e08e8f6820f45382598cc1547
Tom Forbes <tom@tomforb.es>
2022-10-26 13:58:07 +0000

    {"name":"yinpan-fashu-qimen-mianshouban-jiaocai","version":"2022.10.26.0","file":"yinpan-fashu-qimen-mianshouban-jiaocai-2022.10.26.0.tar.gz","path":"yinpan-fashu-qimen-mianshouban-jiaocai/yinpan-fashu-qimen-mianshouban-jiaocai-2022.10.26.0.tar.gz"}

---
+++ yinpan-fashu-qimen-mianshouban-jiaocai/yinpan-fashu-qimen-mianshouban-jiaocai-2022.10.26.0.tar.gz/YinpanFashuQimenMianshoubanJiaocai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yinpan-fashu-qimen-mianshouban-jiaocai
+https://github.com/apachecn/yinpan-fashu-qimen-mianshouban-jiaocai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
