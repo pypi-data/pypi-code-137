f0e878d177dbc4fdeda533beded2bf01c3f0bc16
Tom Forbes <tom@tomforb.es>
2022-10-26 18:03:53 +0000

    {"name":"liyang-fengkuang-yingyu-201407-202001","version":"2022.10.27.0","file":"liyang_fengkuang_yingyu_201407_202001-2022.10.27.0-py3-none-any.whl","path":"liyang-fengkuang-yingyu-201407-202001/liyang_fengkuang_yingyu_201407_202001-2022.10.27.0-py3-none-any.whl"}

---
+++ liyang-fengkuang-yingyu-201407-202001/liyang_fengkuang_yingyu_201407_202001-2022.10.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liyang-fengkuang-yingyu-201407-202001
+https://github.com/apachecn/liyang-fengkuang-yingyu-201407-202001"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
