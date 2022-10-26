015ba3382d641130f0a832ec8903784d1406952c
Tom Forbes <tom@tomforb.es>
2022-10-26 03:00:18 +0000

    {"name":"shidifende-zhuanlan-201906-202101","version":"2022.10.26.0","file":"shidifende_zhuanlan_201906_202101-2022.10.26.0-py3-none-any.whl","path":"shidifende-zhuanlan-201906-202101/shidifende_zhuanlan_201906_202101-2022.10.26.0-py3-none-any.whl"}

---
+++ shidifende-zhuanlan-201906-202101/shidifende_zhuanlan_201906_202101-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shidifende-zhuanlan-201906-202101
+https://github.com/apachecn/shidifende-zhuanlan-201906-202101"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
