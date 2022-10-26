0599be4d2f38e23e3870831d528a18644cee1f6a
Tom Forbes <tom@tomforb.es>
2022-10-26 13:51:54 +0000

    {"name":"taisha-tianshu","version":"2022.10.26.0","file":"taisha-tianshu-2022.10.26.0.tar.gz","path":"taisha-tianshu/taisha-tianshu-2022.10.26.0.tar.gz"}

---
+++ taisha-tianshu/taisha-tianshu-2022.10.26.0.tar.gz/TaishaTianshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taisha-tianshu
+https://github.com/apachecn/taisha-tianshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
