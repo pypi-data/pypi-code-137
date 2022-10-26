828df91cbe6f9554617764592bc8368cd71a4851
Tom Forbes <tom@tomforb.es>
2022-10-26 13:39:47 +0000

    {"name":"chaduitou-haishi-jieduitou","version":"2022.10.26.0","file":"chaduitou_haishi_jieduitou-2022.10.26.0-py3-none-any.whl","path":"chaduitou-haishi-jieduitou/chaduitou_haishi_jieduitou-2022.10.26.0-py3-none-any.whl"}

---
+++ chaduitou-haishi-jieduitou/chaduitou_haishi_jieduitou-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chaduitou-haishi-jieduitou
+https://github.com/apachecn/chaduitou-haishi-jieduitou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
