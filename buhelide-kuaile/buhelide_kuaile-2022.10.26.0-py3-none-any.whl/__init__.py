d40bc3d914733dcc85eceac5bd61ecfa1f68e11a
Tom Forbes <tom@tomforb.es>
2022-10-26 13:39:32 +0000

    {"name":"buhelide-kuaile","version":"2022.10.26.0","file":"buhelide_kuaile-2022.10.26.0-py3-none-any.whl","path":"buhelide-kuaile/buhelide_kuaile-2022.10.26.0-py3-none-any.whl"}

---
+++ buhelide-kuaile/buhelide_kuaile-2022.10.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""buhelide-kuaile
+https://github.com/apachecn/buhelide-kuaile"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
