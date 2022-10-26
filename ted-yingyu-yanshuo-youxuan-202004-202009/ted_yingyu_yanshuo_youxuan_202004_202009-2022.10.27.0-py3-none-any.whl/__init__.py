ab216bdaf7e8f1be06ae895c83da3dff1f2fc151
Tom Forbes <tom@tomforb.es>
2022-10-26 18:16:30 +0000

    {"name":"ted-yingyu-yanshuo-youxuan-202004-202009","version":"2022.10.27.0","file":"ted_yingyu_yanshuo_youxuan_202004_202009-2022.10.27.0-py3-none-any.whl","path":"ted-yingyu-yanshuo-youxuan-202004-202009/ted_yingyu_yanshuo_youxuan_202004_202009-2022.10.27.0-py3-none-any.whl"}

---
+++ ted-yingyu-yanshuo-youxuan-202004-202009/ted_yingyu_yanshuo_youxuan_202004_202009-2022.10.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ted-yingyu-yanshuo-youxuan-202004-202009
+https://github.com/apachecn/ted-yingyu-yanshuo-youxuan-202004-202009"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
