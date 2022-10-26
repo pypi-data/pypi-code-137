6fcf89d7fbc358872682447f609e0426a053d3e1
Tom Forbes <tom@tomforb.es>
2022-10-26 13:56:49 +0000

    {"name":"yangshengyi-haoshui","version":"2022.10.26.0","file":"yangshengyi-haoshui-2022.10.26.0.tar.gz","path":"yangshengyi-haoshui/yangshengyi-haoshui-2022.10.26.0.tar.gz"}

---
+++ yangshengyi-haoshui/yangshengyi-haoshui-2022.10.26.0.tar.gz/YangshengyiHaoshui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yangshengyi-haoshui
+https://github.com/apachecn/yangshengyi-haoshui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
