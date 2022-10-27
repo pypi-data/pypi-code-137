a233330baf1e9ad1f69fa0b1f5880ea969d8aa22
Tom Forbes <tom@tomforb.es>
2022-10-27 14:28:50 +0000

    {"name":"yingyu-shijie-201810-201902","version":"2022.10.27.0","file":"yingyu_shijie_201810_201902-2022.10.27.0-py3-none-any.whl","path":"yingyu-shijie-201810-201902/yingyu_shijie_201810_201902-2022.10.27.0-py3-none-any.whl"}

---
+++ yingyu-shijie-201810-201902/yingyu_shijie_201810_201902-2022.10.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-shijie-201810-201902
+https://github.com/apachecn/yingyu-shijie-201810-201902"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
