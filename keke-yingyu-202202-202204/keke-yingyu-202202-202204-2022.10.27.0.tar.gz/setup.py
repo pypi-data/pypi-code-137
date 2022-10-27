a09475930da417374608bbd595971bbc1f5d73dc
Tom Forbes <tom@tomforb.es>
2022-10-27 13:57:22 +0000

    {"name":"keke-yingyu-202202-202204","version":"2022.10.27.0","file":"keke-yingyu-202202-202204-2022.10.27.0.tar.gz","path":"keke-yingyu-202202-202204/keke-yingyu-202202-202204-2022.10.27.0.tar.gz"}

---
+++ keke-yingyu-202202-202204/keke-yingyu-202202-202204-2022.10.27.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import KekeYingyu202202202204
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('KekeYingyu202202202204'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="keke-yingyu-202202-202204",
+    version=KekeYingyu202202202204.__version__,
+    url="https://github.com/apachecn/keke-yingyu-202202-202204",
+    author=KekeYingyu202202202204.__author__,
+    author_email=KekeYingyu202202202204.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="可可英语 2022.2~2022.4",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "keke-yingyu-202202-202204=KekeYingyu202202202204.__main__:main",
+            "KekeYingyu202202202204=KekeYingyu202202202204.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
