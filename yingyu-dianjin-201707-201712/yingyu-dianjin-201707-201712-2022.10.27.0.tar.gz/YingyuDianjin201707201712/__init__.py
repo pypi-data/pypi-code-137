8ac7b7f15fbdcb625331520ae89d7c8efbec0eef
Tom Forbes <tom@tomforb.es>
2022-10-27 14:20:13 +0000

    {"name":"yingyu-dianjin-201707-201712","version":"2022.10.27.0","file":"yingyu-dianjin-201707-201712-2022.10.27.0.tar.gz","path":"yingyu-dianjin-201707-201712/yingyu-dianjin-201707-201712-2022.10.27.0.tar.gz"}

---
+++ yingyu-dianjin-201707-201712/yingyu-dianjin-201707-201712-2022.10.27.0.tar.gz/YingyuDianjin201707201712/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-dianjin-201707-201712
+https://github.com/apachecn/yingyu-dianjin-201707-201712"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
