d240376af962e68dc9cf26da7c4ec2e3f062cde6
Tom Forbes <tom@tomforb.es>
2022-10-27 13:49:13 +0000

    {"name":"keke-yingyu-201411-201501","version":"2022.10.27.0","file":"keke_yingyu_201411_201501-2022.10.27.0-py3-none-any.whl","path":"keke-yingyu-201411-201501/keke_yingyu_201411_201501-2022.10.27.0-py3-none-any.whl"}

---
+++ keke-yingyu-201411-201501/keke_yingyu_201411_201501-2022.10.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""keke-yingyu-201411-201501
+https://github.com/apachecn/keke-yingyu-201411-201501"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.27.0"
