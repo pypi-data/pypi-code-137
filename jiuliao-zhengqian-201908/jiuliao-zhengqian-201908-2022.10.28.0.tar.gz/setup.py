91d0df87d754f419a0a4a4fd99f4a1161d4d9192
Tom Forbes <tom@tomforb.es>
2022-10-28 04:26:08 +0000

    {"name":"jiuliao-zhengqian-201908","version":"2022.10.28.0","file":"jiuliao-zhengqian-201908-2022.10.28.0.tar.gz","path":"jiuliao-zhengqian-201908/jiuliao-zhengqian-201908-2022.10.28.0.tar.gz"}

---
+++ jiuliao-zhengqian-201908/jiuliao-zhengqian-201908-2022.10.28.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import JiuliaoZhengqian201908
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('JiuliaoZhengqian201908'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="jiuliao-zhengqian-201908",
+    version=JiuliaoZhengqian201908.__version__,
+    url="https://github.com/apachecn/jiuliao-zhengqian-201908",
+    author=JiuliaoZhengqian201908.__author__,
+    author_email=JiuliaoZhengqian201908.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="就聊挣钱知识星球精华 20190828",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "jiuliao-zhengqian-201908=JiuliaoZhengqian201908.__main__:main",
+            "JiuliaoZhengqian201908=JiuliaoZhengqian201908.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
