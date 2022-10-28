af5b71724affe18ede67becc89cbea2dfc6a1add
Tom Forbes <tom@tomforb.es>
2022-10-28 04:51:41 +0000

    {"name":"zeyu-hexinke-201908","version":"2022.10.28.0","file":"zeyu_hexinke_201908-2022.10.28.0-py3-none-any.whl","path":"zeyu-hexinke-201908/zeyu_hexinke_201908-2022.10.28.0-py3-none-any.whl"}

---
+++ zeyu-hexinke-201908/zeyu_hexinke_201908-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zeyu-hexinke-201908
+https://github.com/apachecn/zeyu-hexinke-201908"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
