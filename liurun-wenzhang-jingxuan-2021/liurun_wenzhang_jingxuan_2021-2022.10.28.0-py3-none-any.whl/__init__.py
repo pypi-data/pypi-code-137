92877b1766809274a7d3d48693bd222a0e9f754f
Tom Forbes <tom@tomforb.es>
2022-10-28 04:28:44 +0000

    {"name":"liurun-wenzhang-jingxuan-2021","version":"2022.10.28.0","file":"liurun_wenzhang_jingxuan_2021-2022.10.28.0-py3-none-any.whl","path":"liurun-wenzhang-jingxuan-2021/liurun_wenzhang_jingxuan_2021-2022.10.28.0-py3-none-any.whl"}

---
+++ liurun-wenzhang-jingxuan-2021/liurun_wenzhang_jingxuan_2021-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liurun-wenzhang-jingxuan-2021
+https://github.com/apachecn/liurun-wenzhang-jingxuan-2021"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
