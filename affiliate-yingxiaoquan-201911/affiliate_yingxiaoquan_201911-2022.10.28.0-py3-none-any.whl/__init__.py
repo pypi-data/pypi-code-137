fc7326d72407452802a21ba195ae397697c7202c
Tom Forbes <tom@tomforb.es>
2022-10-28 04:19:18 +0000

    {"name":"affiliate-yingxiaoquan-201911","version":"2022.10.28.0","file":"affiliate_yingxiaoquan_201911-2022.10.28.0-py3-none-any.whl","path":"affiliate-yingxiaoquan-201911/affiliate_yingxiaoquan_201911-2022.10.28.0-py3-none-any.whl"}

---
+++ affiliate-yingxiaoquan-201911/affiliate_yingxiaoquan_201911-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""affiliate-yingxiaoquan-201911
+https://github.com/apachecn/affiliate-yingxiaoquan-201911"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
