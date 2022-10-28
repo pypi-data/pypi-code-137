b87ad00c3baf2421fbfb9425541c5be9e23d7b38
Tom Forbes <tom@tomforb.es>
2022-10-28 04:43:55 +0000

    {"name":"xinwen-shiyanshi-huiyuan-tongxun-vol580","version":"2022.10.28.0","file":"xinwen-shiyanshi-huiyuan-tongxun-vol580-2022.10.28.0.tar.gz","path":"xinwen-shiyanshi-huiyuan-tongxun-vol580/xinwen-shiyanshi-huiyuan-tongxun-vol580-2022.10.28.0.tar.gz"}

---
+++ xinwen-shiyanshi-huiyuan-tongxun-vol580/xinwen-shiyanshi-huiyuan-tongxun-vol580-2022.10.28.0.tar.gz/XinwenShiyanshiHuiyuanTongxunVol580/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinwen-shiyanshi-huiyuan-tongxun-vol580
+https://github.com/apachecn/xinwen-shiyanshi-huiyuan-tongxun-vol580"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
