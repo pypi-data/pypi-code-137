890ae46278192dd8661723b99dcd1b89589fd859
Tom Forbes <tom@tomforb.es>
2022-10-28 04:24:43 +0000

    {"name":"hejiayan-siweidaotu-heji","version":"2022.10.28.0","file":"hejiayan_siweidaotu_heji-2022.10.28.0-py3-none-any.whl","path":"hejiayan-siweidaotu-heji/hejiayan_siweidaotu_heji-2022.10.28.0-py3-none-any.whl"}

---
+++ hejiayan-siweidaotu-heji/hejiayan_siweidaotu_heji-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hejiayan-siweidaotu-heji
+https://github.com/apachecn/hejiayan-siweidaotu-heji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
