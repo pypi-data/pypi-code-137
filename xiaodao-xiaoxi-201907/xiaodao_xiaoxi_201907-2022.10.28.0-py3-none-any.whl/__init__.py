9dd76b6839eab221f5e164e32e7bae5ca3df482d
Tom Forbes <tom@tomforb.es>
2022-10-28 04:42:50 +0000

    {"name":"xiaodao-xiaoxi-201907","version":"2022.10.28.0","file":"xiaodao_xiaoxi_201907-2022.10.28.0-py3-none-any.whl","path":"xiaodao-xiaoxi-201907/xiaodao_xiaoxi_201907-2022.10.28.0-py3-none-any.whl"}

---
+++ xiaodao-xiaoxi-201907/xiaodao_xiaoxi_201907-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiaodao-xiaoxi-201907
+https://github.com/apachecn/xiaodao-xiaoxi-201907"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
