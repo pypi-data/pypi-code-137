1aa7c7466d23face4eb770d08a81e5ccc9f28fce
Tom Forbes <tom@tomforb.es>
2022-10-28 04:33:59 +0000

    {"name":"quanqiu-fengkuang-yanshuo","version":"2022.10.28.0","file":"quanqiu_fengkuang_yanshuo-2022.10.28.0-py3-none-any.whl","path":"quanqiu-fengkuang-yanshuo/quanqiu_fengkuang_yanshuo-2022.10.28.0-py3-none-any.whl"}

---
+++ quanqiu-fengkuang-yanshuo/quanqiu_fengkuang_yanshuo-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""quanqiu-fengkuang-yanshuo
+https://github.com/apachecn/quanqiu-fengkuang-yanshuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
