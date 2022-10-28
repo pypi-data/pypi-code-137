62496ec06de624c4e2df0968a4895bdcc2ecf31e
Tom Forbes <tom@tomforb.es>
2022-10-28 04:19:55 +0000

    {"name":"changzhangde-xiaoshangquan-201910","version":"2022.10.28.0","file":"changzhangde_xiaoshangquan_201910-2022.10.28.0-py3-none-any.whl","path":"changzhangde-xiaoshangquan-201910/changzhangde_xiaoshangquan_201910-2022.10.28.0-py3-none-any.whl"}

---
+++ changzhangde-xiaoshangquan-201910/changzhangde_xiaoshangquan_201910-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""changzhangde-xiaoshangquan-201910
+https://github.com/apachecn/changzhangde-xiaoshangquan-201910"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
