cbb6a96e2662b307dae2c01fe0858164404efae6
Tom Forbes <tom@tomforb.es>
2022-10-28 04:29:52 +0000

    {"name":"liuyuhui-jingji-biji-201906-201908","version":"2022.10.28.0","file":"liuyuhui-jingji-biji-201906-201908-2022.10.28.0.tar.gz","path":"liuyuhui-jingji-biji-201906-201908/liuyuhui-jingji-biji-201906-201908-2022.10.28.0.tar.gz"}

---
+++ liuyuhui-jingji-biji-201906-201908/liuyuhui-jingji-biji-201906-201908-2022.10.28.0.tar.gz/LiuyuhuiJingjiBiji201906201908/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuyuhui-jingji-biji-201906-201908
+https://github.com/apachecn/liuyuhui-jingji-biji-201906-201908"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
