0b9dcac28abf8d49d1a9e3231f0c6e7b4f019f4f
Tom Forbes <tom@tomforb.es>
2022-10-28 04:50:51 +0000

    {"name":"yugong-juejin-jinghua-vol2","version":"2022.10.28.0","file":"yugong-juejin-jinghua-vol2-2022.10.28.0.tar.gz","path":"yugong-juejin-jinghua-vol2/yugong-juejin-jinghua-vol2-2022.10.28.0.tar.gz"}

---
+++ yugong-juejin-jinghua-vol2/yugong-juejin-jinghua-vol2-2022.10.28.0.tar.gz/YugongJuejinJinghuaVol2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yugong-juejin-jinghua-vol2
+https://github.com/apachecn/yugong-juejin-jinghua-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
