2a0760e4c34bc83163d3476ae4f8b8461dda0a74
Tom Forbes <tom@tomforb.es>
2022-10-28 04:37:23 +0000

    {"name":"shouyeren-zongsiling-jiegouxue-202006","version":"2022.10.28.0","file":"shouyeren_zongsiling_jiegouxue_202006-2022.10.28.0-py3-none-any.whl","path":"shouyeren-zongsiling-jiegouxue-202006/shouyeren_zongsiling_jiegouxue_202006-2022.10.28.0-py3-none-any.whl"}

---
+++ shouyeren-zongsiling-jiegouxue-202006/shouyeren_zongsiling_jiegouxue_202006-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shouyeren-zongsiling-jiegouxue-202006
+https://github.com/apachecn/shouyeren-zongsiling-jiegouxue-202006"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
