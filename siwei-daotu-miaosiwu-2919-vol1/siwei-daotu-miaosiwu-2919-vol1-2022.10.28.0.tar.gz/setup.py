92c01e35ddaa871d59674e8371039921a9deedd0
Tom Forbes <tom@tomforb.es>
2022-10-28 04:38:48 +0000

    {"name":"siwei-daotu-miaosiwu-2919-vol1","version":"2022.10.28.0","file":"siwei-daotu-miaosiwu-2919-vol1-2022.10.28.0.tar.gz","path":"siwei-daotu-miaosiwu-2919-vol1/siwei-daotu-miaosiwu-2919-vol1-2022.10.28.0.tar.gz"}

---
+++ siwei-daotu-miaosiwu-2919-vol1/siwei-daotu-miaosiwu-2919-vol1-2022.10.28.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import SiweiDaotuMiaosiwu2919Vol1
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('SiweiDaotuMiaosiwu2919Vol1'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="siwei-daotu-miaosiwu-2919-vol1",
+    version=SiweiDaotuMiaosiwu2919Vol1.__version__,
+    url="https://github.com/apachecn/siwei-daotu-miaosiwu-2919-vol1",
+    author=SiweiDaotuMiaosiwu2919Vol1.__author__,
+    author_email=SiweiDaotuMiaosiwu2919Vol1.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="思维导图妙思屋知识星球整理 2019 第一部分",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "siwei-daotu-miaosiwu-2919-vol1=SiweiDaotuMiaosiwu2919Vol1.__main__:main",
+            "SiweiDaotuMiaosiwu2919Vol1=SiweiDaotuMiaosiwu2919Vol1.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
