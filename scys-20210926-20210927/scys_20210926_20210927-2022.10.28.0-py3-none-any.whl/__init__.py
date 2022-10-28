350078af8e94dc994755bceccd8c9ebc4ea037ab
Tom Forbes <tom@tomforb.es>
2022-10-28 05:25:02 +0000

    {"name":"scys-20210926-20210927","version":"2022.10.28.0","file":"scys_20210926_20210927-2022.10.28.0-py3-none-any.whl","path":"scys-20210926-20210927/scys_20210926_20210927-2022.10.28.0-py3-none-any.whl"}

---
+++ scys-20210926-20210927/scys_20210926_20210927-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""scys-20210926-20210927
+https://github.com/apachecn/scys-20210926-20210927"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
