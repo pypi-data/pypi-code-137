4bb3659860c86d99d9bb3e04e47c1883b735faa1
Tom Forbes <tom@tomforb.es>
2022-10-28 05:16:12 +0000

    {"name":"scys-20210509-20210510","version":"2022.10.28.0","file":"scys-20210509-20210510-2022.10.28.0.tar.gz","path":"scys-20210509-20210510/scys-20210509-20210510-2022.10.28.0.tar.gz"}

---
+++ scys-20210509-20210510/scys-20210509-20210510-2022.10.28.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Scys2021050920210510
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Scys2021050920210510'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="scys-20210509-20210510",
+    version=Scys2021050920210510.__version__,
+    url="https://github.com/apachecn/scys-20210509-20210510",
+    author=Scys2021050920210510.__author__,
+    author_email=Scys2021050920210510.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="生财有术知识星球整理 20210509-20210510",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "scys-20210509-20210510=Scys2021050920210510.__main__:main",
+            "Scys2021050920210510=Scys2021050920210510.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
