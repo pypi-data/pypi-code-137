e25a1029a22ec2a677de9c988d5cdb961767ad66
Tom Forbes <tom@tomforb.es>
2022-10-28 04:41:12 +0000

    {"name":"tongdahuan-2018-caizhi-shuyuan","version":"2022.10.28.0","file":"tongdahuan_2018_caizhi_shuyuan-2022.10.28.0-py3-none-any.whl","path":"tongdahuan-2018-caizhi-shuyuan/tongdahuan_2018_caizhi_shuyuan-2022.10.28.0-py3-none-any.whl"}

---
+++ tongdahuan-2018-caizhi-shuyuan/tongdahuan_2018_caizhi_shuyuan-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tongdahuan-2018-caizhi-shuyuan
+https://github.com/apachecn/tongdahuan-2018-caizhi-shuyuan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
