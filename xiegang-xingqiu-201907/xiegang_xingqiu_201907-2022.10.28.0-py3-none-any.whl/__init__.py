41d75c09487900d0e0076ce9a36ca223588df54a
Tom Forbes <tom@tomforb.es>
2022-10-28 04:43:24 +0000

    {"name":"xiegang-xingqiu-201907","version":"2022.10.28.0","file":"xiegang_xingqiu_201907-2022.10.28.0-py3-none-any.whl","path":"xiegang-xingqiu-201907/xiegang_xingqiu_201907-2022.10.28.0-py3-none-any.whl"}

---
+++ xiegang-xingqiu-201907/xiegang_xingqiu_201907-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiegang-xingqiu-201907
+https://github.com/apachecn/xiegang-xingqiu-201907"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
