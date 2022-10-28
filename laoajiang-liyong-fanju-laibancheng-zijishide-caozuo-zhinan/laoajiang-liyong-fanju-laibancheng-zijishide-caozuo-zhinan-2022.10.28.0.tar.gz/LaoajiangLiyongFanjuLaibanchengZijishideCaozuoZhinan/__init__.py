60ce7554a29247271122972daf7517bc3a47c8d4
Tom Forbes <tom@tomforb.es>
2022-10-28 04:27:05 +0000

    {"name":"laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan","version":"2022.10.28.0","file":"laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan-2022.10.28.0.tar.gz","path":"laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan/laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan-2022.10.28.0.tar.gz"}

---
+++ laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan/laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan-2022.10.28.0.tar.gz/LaoajiangLiyongFanjuLaibanchengZijishideCaozuoZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan
+https://github.com/apachecn/laoajiang-liyong-fanju-laibancheng-zijishide-caozuo-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
