30064ffb19a934925157c0adbbe6419995d29c53
Tom Forbes <tom@tomforb.es>
2022-10-28 04:31:01 +0000

    {"name":"miquan-hudong-heji-2021-shang","version":"2022.10.28.0","file":"miquan_hudong_heji_2021_shang-2022.10.28.0-py3-none-any.whl","path":"miquan-hudong-heji-2021-shang/miquan_hudong_heji_2021_shang-2022.10.28.0-py3-none-any.whl"}

---
+++ miquan-hudong-heji-2021-shang/miquan_hudong_heji_2021_shang-2022.10.28.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""miquan-hudong-heji-2021-shang
+https://github.com/apachecn/miquan-hudong-heji-2021-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
