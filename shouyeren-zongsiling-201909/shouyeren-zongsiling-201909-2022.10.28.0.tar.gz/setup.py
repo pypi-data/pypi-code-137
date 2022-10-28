dc524be0c0b7e4eec050fbf5223f726ad6b65647
Tom Forbes <tom@tomforb.es>
2022-10-28 04:37:16 +0000

    {"name":"shouyeren-zongsiling-201909","version":"2022.10.28.0","file":"shouyeren-zongsiling-201909-2022.10.28.0.tar.gz","path":"shouyeren-zongsiling-201909/shouyeren-zongsiling-201909-2022.10.28.0.tar.gz"}

---
+++ shouyeren-zongsiling-201909/shouyeren-zongsiling-201909-2022.10.28.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ShouyerenZongsiling201909
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ShouyerenZongsiling201909'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="shouyeren-zongsiling-201909",
+    version=ShouyerenZongsiling201909.__version__,
+    url="https://github.com/apachecn/shouyeren-zongsiling-201909",
+    author=ShouyerenZongsiling201909.__author__,
+    author_email=ShouyerenZongsiling201909.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="守夜人总司令知识星球整理 20190916",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "shouyeren-zongsiling-201909=ShouyerenZongsiling201909.__main__:main",
+            "ShouyerenZongsiling201909=ShouyerenZongsiling201909.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
