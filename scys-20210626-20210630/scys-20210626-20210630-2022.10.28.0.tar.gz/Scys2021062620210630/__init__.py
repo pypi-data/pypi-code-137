2a87a288bb1aedd080853a37ed032cbd2942d50f
Tom Forbes <tom@tomforb.es>
2022-10-28 05:19:53 +0000

    {"name":"scys-20210626-20210630","version":"2022.10.28.0","file":"scys-20210626-20210630-2022.10.28.0.tar.gz","path":"scys-20210626-20210630/scys-20210626-20210630-2022.10.28.0.tar.gz"}

---
+++ scys-20210626-20210630/scys-20210626-20210630-2022.10.28.0.tar.gz/Scys2021062620210630/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""scys-20210626-20210630
+https://github.com/apachecn/scys-20210626-20210630"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
