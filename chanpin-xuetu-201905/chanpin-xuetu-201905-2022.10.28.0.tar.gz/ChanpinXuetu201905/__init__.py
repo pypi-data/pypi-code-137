0932d898aab7d7cc243d644d9e745efe043e41ed
Tom Forbes <tom@tomforb.es>
2022-10-28 04:20:06 +0000

    {"name":"chanpin-xuetu-201905","version":"2022.10.28.0","file":"chanpin-xuetu-201905-2022.10.28.0.tar.gz","path":"chanpin-xuetu-201905/chanpin-xuetu-201905-2022.10.28.0.tar.gz"}

---
+++ chanpin-xuetu-201905/chanpin-xuetu-201905-2022.10.28.0.tar.gz/ChanpinXuetu201905/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chanpin-xuetu-201905
+https://github.com/apachecn/chanpin-xuetu-201905"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.28.0"
