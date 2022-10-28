78c8a41bc767bdab6803c024d2e97229654380cd
Tom Forbes <tom@tomforb.es>
2022-10-28 04:36:43 +0000

    {"name":"shiyidian-ganggu-daxin-201909","version":"2022.10.28.0","file":"shiyidian-ganggu-daxin-201909-2022.10.28.0.tar.gz","path":"shiyidian-ganggu-daxin-201909/shiyidian-ganggu-daxin-201909-2022.10.28.0.tar.gz"}

---
+++ shiyidian-ganggu-daxin-201909/shiyidian-ganggu-daxin-201909-2022.10.28.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ShiyidianGangguDaxin201909
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ShiyidianGangguDaxin201909'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="shiyidian-ganggu-daxin-201909",
+    version=ShiyidianGangguDaxin201909.__version__,
+    url="https://github.com/apachecn/shiyidian-ganggu-daxin-201909",
+    author=ShiyidianGangguDaxin201909.__author__,
+    author_email=ShiyidianGangguDaxin201909.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="11 点港股打新知识星球整理 20190909",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "shiyidian-ganggu-daxin-201909=ShiyidianGangguDaxin201909.__main__:main",
+            "ShiyidianGangguDaxin201909=ShiyidianGangguDaxin201909.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
